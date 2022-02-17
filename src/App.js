46b80b8d7d98074be13c3b0d003b7fdacc155e18
Solar5503 <spidervvv@mail.ru>
2022-02-17 17:42:04 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter';
+import './styles/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = useState('Текст в инпуте');
+
+  return (
+    <div className="App">
+      {/* <ClassCounter />
+      <Counter /> */}
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>Javascript - язык программирования</div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
