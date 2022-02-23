26127f176bcafd2101ff50ad8298b9b594fd6292
solar <spidervvv@mail.ru>
2022-02-23 21:55:42 +0300

    add navigation + comments

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,6 +1,7 @@
 import MyButton from './UI/button/MyButton';
-
+import { useNavigate } from 'react-router-dom';
 const PostItem = (props) => {
+  const navigate = useNavigate();
   return (
     <div className="post">
       <div className="post__content">
@@ -10,6 +11,9 @@ const PostItem = (props) => {
         <div>{props.post.body}</div>
       </div>
       <div className="post__btns">
+        <MyButton onClick={() => navigate(`/posts/${props.post.id}`)}>
+          Открыть
+        </MyButton>
         <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
       </div>
     </div>
